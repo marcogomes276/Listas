c4cc3ef226b73c5f4744f837dac9f485b45ac75f
Vai na Web <vainawebcpx@gmail.com>
2018-02-28 16:54:21 -0300

    cores ja se foram

--- src/components/Listas.vue
+++ src/components/Listas.vue
@@ -1,41 +1,69 @@
 <template>
+
 	<div id="html-principal">
+	
 		<main class="main">
+			
 			<section class="sec">
+				
 				<form  class="form">
-					<div tituloesubtitulo>
+					
+					<div class="A=titulos">
+			
 						<h1 class="titulo">Listas de alunos do vnw</h1>
 						<h2 class="subtitulo">modulo 2</h2>
+					
 					</div>
-					<div>
+					
+					<div class="B=usuario">
+					
 						<label>Ordenar por:</label>
+					
 						<select class="form-control" v-model="configs.orderBy">
+        				
         					<option value="name">Nome</option>				
         					<option value="age">Idade</option>
+					
 					</select>
+					
 					</div>
-					<div>
+					
+					<div class="C=cresente">
+						
 						<label>Orderm:</label>
+					    
 					    <select class="form-control" v-model="configs.order">
+					        
 					        <option value="asc">Crescente</option>
-					        <option value="desc">Decrescente</option>					 
+					        <option value="desc">Decrescente</option> 
+					
 					    </select>
+					
 					</div>
-					<div class="form-group">
+					
+					<div class="Filtrar">
+				      
 				      <label>Filtrar</label>
+				      
 				      <input
 				      	type="text" 
-				      	class="form-control"
+				      	class="excrever"
 				      	placeholder="Filtrar por nome"
 				      	v-model="configs.filter">
-				    </div>
-				</form>
-			</section>
-			<Alunos :list="list"></Alunos>
-		</main>
-	</div>	
-</template>
 
+				    </div>
+
+				</form>
+
+			</section>
+
+			<Alunos :list="list"></Alunos>
+
+		</main>
+
+	</div>
+
+</template>
 <script>
 import _ from 'lodash'
 import Alunos from './Alunos.vue'
@@ -92,22 +120,34 @@ export default{
 
 <style scoped>
 #html-principal{
-	background: black;
-	color: white;
+	background: white;
+	color: #6D90EA;
 	height: 80vh;
 	margin:0;
 	padding: 0;
 }
-.tituloesubtitulo{
+.sec{
+	margin: 0;
+	padding: 0;
+	background-color: white;
+}
+.main{
+	margin: 0;
+	padding: 0;
+	background-color: white;
+}
+.A{
 	margin: 0;
 	padding: 0;
 }
 .titulo{
 	margin: 0;
-	padding: 0;	
+	padding: 0;
+	color: #6D90EA;	
 }
 .subtitulo{
 	margin: 0;
 	padding: 0;
+	color:  #DEE3F5;
 }
 </style>
